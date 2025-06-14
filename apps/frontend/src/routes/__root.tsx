81ec0640f5d25d63d5fcefee1e33c84e22b34972
Shanthosh <shanthubolt@gmail.com>
2025-06-14 20:58:59 +0530

    Initial commit

---
+++ apps/frontend/src/routes/__root.tsx
@@ -0,0 +1,33 @@
+import {
+  Outlet,
+  createRootRouteWithContext,
+  useRouterState,
+} from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import type { QueryClient } from '@tanstack/react-query'
+import { AuthProvider } from '@/lib/context/AuthContext'
+import { ChatProvider } from '@/lib/context/ChatContext'
+
+interface ReactContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<ReactContext>()({
+  component: () => {
+    const location = useRouterState({ select: (s) => s.location.pathname })
+    const isAuthPage = /^\/(login|signup)/i.test(location)
+    const validateSession = !isAuthPage
+
+    // Handle redirect on root pat
+    return (
+      <>
+        <AuthProvider validateSession={validateSession}>
+          <ChatProvider>
+            <Outlet />
+          </ChatProvider>
+          <TanStackRouterDevtools />
+        </AuthProvider>
+      </>
+    )
+  },
+})
---
+++ apps/server/src/app.ts
@@ -0,0 +1,72 @@
+import config from "./config/config";
+import express from "express";
+import originRoutes from "./originRoutes";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import passport from "passport";
+import cors from "cors";
+import { getMongoClient } from "./lib/mongoClient";
+import validateUserSession from "./middleware/validateSession";
+import errorHandler from "./middleware/errorHandler";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+const clientPromise = getMongoClient();
+
+app.use(
+  // (req, res, next) => {
+  //   res.setHeader("Access-Control-Allow-Origin", config.frontend_url);
+  //   res.setHeader("Access-Control-Allow-Credentials", "true");
+  //   next();
+  // },
+  cors({
+    origin: [config.frontend_url, "http://localhost:3001"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "Origin",
+      "Accept",
+    ],
+  })
+);
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    store: MongoStore.create({
+      clientPromise: clientPromise,
+      collectionName: "sessions",
+      ttl: 24 * 60 * 60,
+    }),
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+      secure: config.NODE_ENV === "production",
+    },
+  })
+);
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+app.get("/check", (req, res) => {
+  // console.log(config.frontend_url);
+
+  res.status(200).send("Working");
+});
+
+app.use("/", validateUserSession, originRoutes);
+
+app.use(errorHandler);
+export default app;
